18dae3004ba69a066254a1941757f96f2a7e5bb9
Josilandio <landderson@proton.me>
2024-10-08 00:12:25 -0300

    add: neovim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+-- Fonts (plugin 'nvim-web-devicons').
+vim.cmd([[ set guifont=JetBrains\ Mono:h10 ]])
+
+-- 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+
+-- Colors.
+vim.opt.termguicolors = true
+
+-- Clipboard.
+vim.opt.clipboard = 'unnamedplus'
